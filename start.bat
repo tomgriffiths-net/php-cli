3faa21193339cb9225a2d47aff753f05880d3561
Tom <tomgriffiths2007@gmail.com>
2025-06-10 12:36:35 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,8 +15,20 @@ if exist cli.php (
         echo Downloading PHP
         powershell -command "$ProgressPreference = 'SilentlyContinue'; $link = (Invoke-WebRequest -Uri https://files.tomgriffiths.net/php-cli/php_latest.txt).Content; Invoke-WebRequest -OutFile php.zip -Uri $link"
         
+        if not exist php.zip (
+            echo Failed to download PHP
+            pause
+            exit
+        )
+
         echo Installing PHP
         powershell -command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path php.zip -DestinationPath ./php"
+
+        if not exist php\php.exe (
+            echo Failed to unzip PHP
+            pause
+            exit
+        )
         
         del php.zip
         echo Launching CLI
