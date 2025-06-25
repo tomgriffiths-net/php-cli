6804a7500ac4010d71b4d70dba4c84acb8e982d3
Tom <tomgriffiths2007@gmail.com>
2025-06-25 19:43:43 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
 @echo off
+pause
+
 cd /D "%~dp0"
 echo Downloading php-cli latest
 powershell -command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -OutFile latest.zip -Uri https://files.tomgriffiths.net/php-cli/updates/latest.zip; Expand-Archive -Path latest.zip -DestinationPath ."
