0fef9ed2d59266f310556d2bf02b9548dc5e5f34
Tom <tomgriffiths2007@gmail.com>
2025-09-13 15:10:06 +0100

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pause
-
-cd /D "%~dp0"
-echo Downloading php-cli latest
-powershell -command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -OutFile latest.zip -Uri https://files.tomgriffiths.net/php-cli/updates/latest.zip; Expand-Archive -Path latest.zip -DestinationPath ."
-del latest.zip
-start /b "" cmd /c del "%~f0"&exit /b
